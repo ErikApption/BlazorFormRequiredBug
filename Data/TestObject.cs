0b15b18c1f157b69b03418805b06a70ab03497b0
Erik Putrycz <erik@apption.com>
2020-11-27 13:05:46 -0500

    Add project files.

---
+++ Data/TestObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorFormRequiredBug.Data
+{
+    public class TestObject
+    {
+        [Required]
+        public string MyCrypticProperty { get; set; }
+    }
+}
